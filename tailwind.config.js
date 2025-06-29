2e23d27932548e58a536c514539b46959230c820
Angelo Mendoza <angelo@modiphy.net>
2025-06-29 23:17:36 +0800

    💥

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        garet: ["var(--font-garet)", "sans-serif"], // Use the CSS variable
+        brittany: ["var(--font-brittany)", "serif"], // Use the CSS variable
+      },
+    },
+  },
+};
